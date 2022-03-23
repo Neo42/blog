fcd1c56ee170678bbd5657844164ea38eaebcbee
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-03-24 03:58:25 +0800

    feat: 💄 hide my avatar

--- components/post-header.js
+++ components/post-header.js
@@ -7,14 +7,14 @@ export default function PostHeader({title, coverImage, date, author}) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="flex items-center justify-between">
+      {/* <div className="flex items-center justify-between">
         <div className="block mb-6">
           <Avatar name={author.name} picture={author.picture} />
         </div>
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
-      </div>
+      </div> */}
       {/* <div className="hidden md:block md:mb-12">
         <Avatar name={author.name} picture={author.picture} />
       </div> */}
--- components/post-preview.js
+++ components/post-preview.js
@@ -2,19 +2,19 @@ import Avatar from '../components/avatar'
 import DateFormatter from '../components/date-formatter'
 import Link from 'next/link'
 
-export default function PostPreview({ title, date, excerpt, author, slug }) {
+export default function PostPreview({title, date, excerpt, author, slug}) {
   return (
     <div className="my-40">
-      <h3 className="text-3xl mb-3 leading-snug font-bold">
+      <h3 className="mb-3 text-3xl font-bold leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+      <div className="mb-4 text-lg">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      <p className="mb-4 text-lg leading-relaxed">{excerpt}</p>
+      {/* <Avatar name={author.name} picture={author.picture} /> */}
     </div>
   )
 }
