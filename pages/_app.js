0e723d657eea64edc99b7c7f998b94824ce70b9e
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-03-24 03:51:47 +0800

    fix: 🐛 fix code font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '@code-hike/scrollycoding/dist/index.css'
+import '../styles/dank-mono.css'
 import '../styles/pmndrs.css'
 import '../styles/index.css'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
 }
